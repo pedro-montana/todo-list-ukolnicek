0342ab41d94cdb0578ebeadf78d8626ab38c53f9
Petr Horacek <petrhoracek02@gmail.com>
2020-11-27 10:32:07 +0100

    Added styling and context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { Header } from './components/layout/Header';
-import { Content } from './components/layout/Content';
+import { Header } from "./components/layout/Header";
+import { Content } from "./components/layout/Content";
+import "./App.scss";
+import { ProjectsProvider, SelectedProjectProvider } from "./context";
 
-export const App = () => (
-  <div className="App">
-    <Header />
-    <Content />
-  </div>
-);
+export const App = () => {
+  return (
+    <SelectedProjectProvider>
+    <ProjectsProvider>
+      <div className="App">
+        <Header />
+        <Content />
+      </div>
+    </ProjectsProvider>
+    </SelectedProjectProvider>
+  );
+};
